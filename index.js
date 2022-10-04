263c166b248cc7b551521ab609ba041f71c6030d
Colleen Cabedo <cmcabedo@gmail.com>
2022-10-04 15:40:38 -0400

    bootstrap

--- index.js
+++ index.js
@@ -1,45 +1,36 @@
-fetch("http://localhost:3000/pokemon")
-.then(res => res.json())
-.then(data => {
-data.forEach(character => {
-    renderData(character)
-})
-})
-renderData = (eachPokemon) => {
-        const ul = document.querySelector("#pokemon-list")
-        const li = document.createElement("li")
-        li.innerText = eachPokemon.name
-        ul.append(li)
+// const url = "http://localhost:3000/pokemon"
 
-        li.addEventListener("click", e => {
-            e.preventDefault()
-            const characterName = document.querySelector("#character-name")
-            characterName.innerText = eachPokemon.name
+// fetch(url)
+// .then(res => res.json())
+// .then(data => data.forEach(el => renderData(el)))
 
-            const imageDiv = document.querySelector("#image")
-            const image = document.createElement("img")
-            image.src = eachPokemon.img
-            imageDiv.append(image)
+// function renderData (pokemon) {
+//     const ul = document.querySelector("#pokemon-list")
+//     const li = document.createElement("li")
+//     li.innerText = pokemon.name
+//     ul.append(li)
+//     li.addEventListener("click", () => displayPokemon())
+// }
 
-            const descriptionDiv = document.querySelector("#description")
-            const pTag = document.createElement("p")
-            pTag.innerText = eachPokemon.description
-            descriptionDiv.append(pTag)
+// function displayPokemon(details){
+//     const characterName = document.querySelector("#character-name")
+//     characterName.innerText = details.name
 
-            const formDiv = document.querySelector("#form")
-            const ulComment = document.createElement("ul")
-            const liCommentList = document.createElement("li")
-            formDiv.append(ulComment, liCommentList)
-            liCommentList.innerText = eachPokemon.comment
+//     const imageDiv = document.querySelector("#image")
+//     const image = document.createElement("img")
+//     image.src = details.img
+//     imageDiv.append(image)
 
-        formDiv.addEventListener("submit", e => {
-            e.preventDefault()
-            console.log(e)
+//     const descriptionDiv = document.querySelector("#description")
+//     const pTag = document.createElement("p")
+//     pTag.innerText = details.description
+//     descriptionDiv.append(pTag)
 
-            // const commentValue = document.querySelector("#comment")
-            // formDiv.innerText = commentValue.value
+//     const formDiv = document.querySelector("#form")
+//     const ulComment = document.createElement("ul")
+//     const liCommentList = document.createElement("li")
+//     formDiv.append(ulComment, liCommentList)
+//     liCommentList.innerText = details.comment
+// }
 
-
-        })
-        })
-    }
+// displayPokemon()
